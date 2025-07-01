9576c29b176c5f75bd70c3da33e69c3a8344f2c0
Mohamed Mahmoud AbdElsalam Behiry <mmbehiry@gmail.com>
2025-07-01 08:18:42 +0300

    Q12

--- AssignmentFour/Program.cs
+++ AssignmentFour/Program.cs
@@ -209,7 +209,55 @@ namespace AssignmentFour
 
             #endregion
 
-            #region
+            #region 12- Write a program to create a Simple Calculator.
+            //double num1, num2, result = 0;
+            //char operation;
+
+            //Console.Write("Enter first number: ");
+            //num1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Enter an operator (+, -, *, /): ");
+            //operation = Convert.ToChar(Console.ReadLine());
+
+            //Console.Write("Enter second number: ");
+            //num2 = Convert.ToDouble(Console.ReadLine());
+
+            //bool validOperation = true;
+
+            //switch (operation)
+            //{
+            //    case '+':
+            //        result = num1 + num2;
+            //        break;
+
+            //    case '-':
+            //        result = num1 - num2;
+            //        break;
+
+            //    case '*':
+            //        result = num1 * num2;
+            //        break;
+
+            //    case '/':
+            //        if (num2 != 0)
+            //            result = num1 / num2;
+            //        else
+            //        {
+            //            Console.WriteLine("Error: Cannot divide by zero.");
+            //            validOperation = false;
+            //        }
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Invalid operator.");
+            //        validOperation = false;
+            //        break;
+            //}
+
+            //if (validOperation)
+            //{
+            //    Console.WriteLine("Result: " + result);
+            //}
             #endregion
 
             #region
